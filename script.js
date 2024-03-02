092c84aeb9fceaf96ea53e6d0eadf617a94ce9c8
Nathaniyell <essien.nathan@yahoo.com>
2024-03-02 10:51:52 +0100

    update script.js and style.css

--- script.js
+++ script.js
@@ -165,8 +165,14 @@ $(document).ready(function () {
     placeholder: "בחירת מרכז",
     // allowClear: true,
   });
-  $("#openDropdownButton").click(function () {
+  $("#openDropdownButton").click(function (e) {
     var $select2 = $("#input_3").data('select2');
+    if(e.target.classList.contains("select2-container--open")){
+      $("#input_3").select2('close');
+    }
+    else if(e.target.classList.contains("select2-container")){
+      $("#input_3").select2('open');
+    }
   });
   $("#input_3").on("change", function () {
     handleChange();
